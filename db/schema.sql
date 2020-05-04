eee12fa46eea5cb86834eea190c7418f312449d0
Kyle Tov <32151304+kyletov@users.noreply.github.com>
2020-05-04 17:26:27 -0400

    Add Project Files

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+--- load with 
+--- sqlite3 database.db < schema.sql
+
+CREATE TABLE appuser (
+	id VARCHAR(20),
+	password VARCHAR(20),
+	name VARCHAR(20)
+);
+
+
---
+++ ww_node.js
@@ -0,0 +1,30 @@
+require('./port');
+var express = require('express');
+var app = express();
+
+// http://www.sqlitetutorial.net/sqlite-nodejs/connect/
+const sqlite3 = require('sqlite3').verbose();
+
+// https://scotch.io/tutorials/use-expressjs-to-get-url-and-post-parameters
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// http://www.sqlitetutorial.net/sqlite-nodejs/connect/
+// will create the db if it does not exist
+var db = new sqlite3.Database('db/database.db', (err) => {
+	if (err) {
+		console.error(err.message);
+	}
+	console.log('Connected to the database.');
+});
+
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static('static-content')); 
+
+app.listen(port, function () {
+  	console.log('Example app listening on port '+port);
+});
+
+// db.close();
+
